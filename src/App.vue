4cdedfbf334e384291cf334625e552d0cee4ca78
Abdullah Aljubran <abdullahaljubran@Abdullahs-iMac.local>
2023-09-17 21:02:00 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TextEditor/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TextEditor from "@/components/TextEditor.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TextEditor,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  background-color: #FFDA33;
+  color: #FFFFFF;
+  font-family: 'Poppins';
 }
 </style>
