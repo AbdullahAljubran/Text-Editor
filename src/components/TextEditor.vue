c4c6977d40bf7a98582e11ce6aa6653320175611
AbdullahAljubran <3abdullah.cx@gmail.com>
2023-09-20 19:56:38 +0300

    Second Commit

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -1,6 +1,10 @@
 <template>
-  <div id="title" class="d-flex align-items-center justify-content-center">Text Editor</div>
-    <div v-if="editor" class="d-flex align-items-center justify-content-center">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div id="title" class="d-flex align-items-center justify-content-center">Text Editor</div>
+        <div v-if="editor" class="d-flex align-items-center justify-content-center">
+          <div class="btn-group" role="group">
       <button @click="editor.chain().focus().toggleBold().run()" :disabled="!editor.can().chain().focus().toggleBold().run()" :class="{ 'is-active': editor.isActive('bold') }">
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-type-bold" viewBox="0 0 16 16">
           <path d="M8.21 13c2.106 0 3.412-1.087 3.412-2.823 0-1.306-.984-2.283-2.324-2.386v-.055a2.176 2.176 0 0 0 1.852-2.14c0-1.51-1.162-2.46-3.014-2.46H3.843V13H8.21zM5.908 4.674h1.696c.963 0 1.517.451 1.517 1.244 0 .834-.629 1.32-1.73 1.32H5.908V4.673zm0 6.788V8.598h1.73c1.217 0 1.88.492 1.88 1.415 0 .943-.643 1.449-1.832 1.449H5.907z"/>
@@ -83,7 +87,11 @@
         </svg>
       </button>
     </div>
-    <editor-content :editor="editor" class="d-flex align-items-center justify-content-center"/>
+        </div>
+      </div> 
+    </div>
+  </div>
+  <editor-content :editor="editor" class="d-flex align-items-center justify-content-center" />
   </template>
   
   <script>
@@ -167,5 +175,15 @@ button:hover {
 .pre-container pre {
   display: block; 
   text-align: left;
+}
+@media (max-width: 768px) {
+  #title {
+    font-size: 24px; /* Adjust font size for smaller screens */
+  }
+
+  button {
+    width: 40px; /* Adjust button size for smaller screens */
+    
+  }
 }
   </style>
